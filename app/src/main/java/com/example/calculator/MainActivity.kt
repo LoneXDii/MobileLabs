07883066d973200b6e2d5a59d1d43604992f7a16
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2025-01-30 13:48:06 +0300

    Calculator base was rebuilded

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,25 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
-import Application.ViewModels.CalculatorViewModel
-import Presentation.Calculator
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.calculator.Application.ViewModel.CalculatorViewModel
+import com.example.calculator.Presentation.Calculator
 import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val calculatorViewModel = ViewModelProvider(this)[CalculatorViewModel::class.java]
         enableEdgeToEdge()
         setContent {
             CalculatorTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Calculator(modifier = Modifier.padding(innerPadding), calculatorViewModel)
-                }
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state =viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
